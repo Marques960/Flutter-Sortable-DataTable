a4d3388846d94b360f76558b7f4e8b90af59e0d7
Marques960 <rafaeltavaresmarques06@gmail.com>
2024-06-13 12:43:14 +0100

    tabke style

---
+++ sortable_table/lib/Styles/table_style.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+TextStyle tableStyle = TextStyle(
+  fontWeight: FontWeight.w400,
+  color: Colors.black,
+  fontSize: 15,
+);
