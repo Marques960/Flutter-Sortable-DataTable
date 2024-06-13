b3f1853d6a4d4afa7709a5d258278ec33c902f2b
Marques960 <rafaeltavaresmarques06@gmail.com>
2024-06-13 11:18:58 +0100

    creation of page table

---
+++ sortable_table/lib/Table/table.dart
@@ -0,0 +1,25 @@
+//ignorances
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+//imports
+import 'package:flutter/material.dart';
+
+class TablePage extends StatefulWidget {
+  const TablePage({super.key});
+
+  @override
+  State<TablePage> createState() => _TableState();
+}
+
+class _TableState extends State<TablePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
